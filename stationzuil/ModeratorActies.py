c1de0a5d45814bbba26ab1682ad6b5a91765b3f2
JefrDev <Jeroen.fredriksz@gmail.com>
2023-10-21 15:55:52 +0200

    logica voor het keuren van een bericht gemaakt. het sturen van het bericht naar de database is nog niet uitgewerkt

--- stationzuil/ModeratorActies.py
+++ stationzuil/ModeratorActies.py
@@ -1,5 +1,3 @@
-import datetime
-
 import DatabaseEditor
 
 
@@ -8,17 +6,29 @@ def modereerBerichten():
                           ": ")
     moderatorEmail = input("Wat is uw email?\n"
                            ": ")
+
     lines = DatabaseEditor.readDatabase()
 
     for line in lines:
         splittedLines = line.split("/")
         naam = splittedLines[0]
         datum = splittedLines[1]
-        datetime.datetime.fromtimestamp(datum)
         bericht = splittedLines[2]
         station = splittedLines[3].strip()
 
+        print(f"Het huidige bericht om te keuren is:\n"
+              f'"{bericht}"\n'
+              f'geschreven door {naam} op {datum} op station {station}\n\n'
+              f'"wilt u dit bericht goedkeuren? antwoord "Ja" of "Nee"')
 
+        # while loop zodat als moderator geen ja of nee opgeeft, het programma niet doorgaat met een ongeldig antwoord
+        while True:
+            keuring = input(": ").lower()
+            if keuring == "ja" or keuring == "nee":
+                break
+            else:
+                print('Dit is geen geldige invoer! Voer in "Ja" of "Nee"')
+#             zo nee, gaat het terug naar vragen om keuring input
 
 
 modereerBerichten()
